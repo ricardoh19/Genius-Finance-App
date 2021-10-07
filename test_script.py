891da88aa2750fac3901ffc11251896594936fee
Louise Schmidtgen <louiseschmidtgen@Louises-MacBook-Pro.local>
2021-10-06 22:05:10 -0400

    test

--- test_script.py
+++ test_script.py
@@ -6,14 +6,15 @@ def test_yahoo_api():
     #Test 1 create object
     YahooAPIObject = YahooAPI()
     #Test 2 get watchlist info
-    # YahooAPIObject.get_watchlist_info(["TSLA", "APPL"])
+    YahooAPIObject.get_watchlist_info(["TSLA", "APPL"])
     #Test 2.1 get watchlist info wrong value
-    # YahooAPIObject.get_watchlist_info(["AAAA"])
+    YahooAPIObject.get_watchlist_info(["AAAA"])
     #Test 3 get link
-    # YahooAPIObject.get_link(stocksymbol="TSLA")
-    # YahooAPIObject.get_watchlist_info(["AAAA"])
+    YahooAPIObject.get_link(stocksymbol="TSLA")
+    YahooAPIObject.get_watchlist_info(["AAAA"])
     #Test 4 get Graph data
     YahooAPIObject.get_stock_graph(stocksymbol="TSLA")
+    YahooAPIObject.get_stock_graph(stocksymbol="AAAA")
 
 
 if __name__ == "__main__":
