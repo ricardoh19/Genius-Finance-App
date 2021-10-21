b6a8f6c5846c77783ea75db192d988444945b665
Ricardo Hernandez <hs.ricardoj@gmail.com>
2021-10-20 20:10:31 -0400

    updated dashboard controller and GUI

--- dashboardGUI.py
+++ dashboardGUI.py
@@ -1,14 +1,45 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+import dashboard_controller
+
 
 class DashboardGUI():
-    def __init__(self, DashboardControllerObject,popup_GUI_object):
-        self.DashboardControllerObject = DashboardControllerObject
-        self.popup_GUI_object = popup_GUI_object
+    def __init__(self, master):
+        self.dashboardControllerObject = dashboard_controller.DashboardController()
+        #self.popup_GUI_object = popup_GUI_object
+        self.master = master
+        self.master.title("Dashboard")
+        self.createMainFrame()
 
-    def handle_logout_event(self):
-        """Logout button is pressed. 
-        Tells Dashboard controller to do logout processing"""
-        self.DashboardControllerObject.handle_logout_event()
-        #TO:DO frame destroyed
+
+    def createMainFrame(self):
+        self.logo = Label(self.master, text="Genius Finance",font='Helvetica 12',height = 6, width = 13,borderwidth=2, relief="solid").grid(row=0,column=0, pady=5, padx=5)
+        self.createWatchlistPortfolioFrame()
+        self.exitButton = Button(self.master,text="Exit", command=lambda:self.closeWindow()).grid(row = 4,column=1,sticky="se")
+
+    
+    def createSearchbarFrame(self):
+        pass
+
+    def createWatchlistPortfolioFrame(self):
+        self.buttons= Frame(self.master, width = 500, height = 400, borderwidth=2, relief="sunken").grid(row = 1,column=1,pady=4)
+
+    """Logout button is pressed. 
+    Tells Dashboard controller to do logout processing"""
+    def handleLogoutEvent(self):
+        self.DashboardControllerObject.handleLogoutEvent()
+        self.closeWindow()
+        self.loginlogout_ControllerObject.createLoginGUI()
+    
+
+
+
+    def handle_watchlist_event(self):
+        pass
+    def handle_portfolio_event(self):
+        """Calls Dashboard Controller to create portfolio"""
+        self.DashboardControllerObject.create_portfolio_controller()
 
     def handle_searchbar_event(self):
         """Gets stock_symbol selected and passes it on 
@@ -16,18 +47,6 @@ class DashboardGUI():
         stock_symbol = "" #ToDo
         #call Dashboard controller function to process search bar stock_symbol
         self.DashboardControllerObject.handle_search_bar_event(stock_symbol)
-    
-    def handle_watchlist_event():
 
-        pass
-    def handle_portfolio_event(self):
-        """Calls Dashboard Controller to create portfolio"""
-        self.DashboardControllerObject.create_portfolio_controller()
-
-    def CreateMainFrame():
-        pass
-    def CreateSearchbarFrame():
-        pass
-    def CreateWatchlistPortfolioFrame():
-        pass
-    
+    def closeWindow(self):
+        self.master.destroy()
--- dashboard_controller.py
+++ dashboard_controller.py
@@ -1,31 +1,34 @@
 from stock_controller import StockController
 from yahoo_api import YahooAPI
-from dashboardGUI import DashboardGUI
+from tkinter import *
+import loginlogout_controller
 from portfolio_controller import PortfolioController
 from watchlist_controller import WatchlistController
+import user
+import popupGUI
 
 class DashboardController():
     """This class is the intersection of APP traffic after login. """
-    def __init__(self, login_logout_controller, user_object,popup_GUI_object):
-        #passed in objects
-        self.login_logout_controller = login_logout_controller
-        self.user_object = user_object
-        self.popup_GUI_object = popup_GUI_object
+    def __init__(self):
+        self.login_logout_controller = loginlogout_controller
+        #self.user_object = user.User()
+        
         #created in this class objects
         self.watchlist_object = None 
         self.stock_controller_object= None #replaced by stock object
         self.portfolio_object = None
         #created during initialization object
         self.yahoo_api_object = YahooAPI() # is created
-        #create dashboard GUI
-        self.create_dashboard_GUI   
+        
+        
         
     
-    def handle_logout_event(self):
+    def handleLogoutEvent(self):
         """In Dashboard GUI logout button is pressed.
         The controller passes the control back to login/logout controller.
         Login logout controller function is called which pushes the changes to the database"""
         self.login_logout_controller.logout_push_changes_to_database(self.user_object)
+        
 
     def create_stock_controller(self, stock_symbol):
         """if stock controller object does not exist creates it.
@@ -49,9 +52,13 @@ class DashboardController():
            self.portfolio_object =  PortfolioController(self.user_object,self,self.yahoo_api_object, self.stock_controller_object, self.yahoo_api_object) 
         self.portfolio_object.create_portfolio_GUI_object()
         
-    def create_dashboard_GUI(self):
-        """This function creates the Dashboard GUI Object"""
-        self.DashboardGUIObject = DashboardGUI(self,self.popup_GUI_object)
+    """This function creates the Dashboard GUI Object"""
+    def createDashboardGUI(self):
+        #self.DashboardGUIObject.create
+        root = Tk()
+        root.geometry("675x600")
+        self.dashboardGUIObject = dashboard_controller.dashboardGUI(root)
+        root.mainloop()
 
     def handle_search_bar_event(self, stock_symbol):
         """Checks if the stock_symbol given in the search bar actually exists.
@@ -66,7 +73,7 @@ class DashboardController():
             message = "Could not find stock entered in search bar."
             self.create_popup_GUI(message)
 
-    def create_popup_GUI(self, message):
+    def createPopUpGUI(self, message):
         """creates a pop-up GUI with given error message."""
         self.popup_GUI_object.create_pop_up(message)
       
