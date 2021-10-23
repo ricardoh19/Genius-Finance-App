753264348e540f3e6cf05faefb5bbefeea4bfbd7
Ricardo Hernandez <hs.ricardoj@gmail.com>
2021-10-23 10:35:56 -0400

    Updated dashboard GUI

--- dashboardGUI.py
+++ dashboardGUI.py
@@ -12,18 +12,39 @@ class DashboardGUI():
         self.master.title("Dashboard")
         self.createMainFrame()
 
-
+    '''
+    Intent: creates the main frame for the dashboard GUI
+    * Preconditions: master is connected to TKinter. 
+    * createWatchlistPortfolioFrame and createSearchbarFrame have the appropriate GUI code to be called in this method.
+    * Postconditions:
+    * Post0. main frame for dashboard is created
+    '''
     def createMainFrame(self):
         self.logo = Label(self.master, text="Genius Finance",font='Helvetica 12',height = 6, width = 13,borderwidth=2, relief="solid").grid(row=0,column=0, pady=5, padx=5)
         self.createWatchlistPortfolioFrame()
+        self.createSearchbarFrame()
         self.exitButton = Button(self.master,text="Exit", command=lambda:self.closeWindow()).grid(row = 4,column=1,sticky="se")
 
     
+    '''
+    Intent: creates the search bar frame for the login GUI
+    * Preconditions: master is connected to TKinter.
+    * Postconditions:
+    * Post0. search bar frame is created.
+    '''
     def createSearchbarFrame(self):
+        self.inputtxt = Text(self.master, height = 2, width = 25, bg = "light yellow").grid(row = 2,column=4,sticky="ne")
+
+
+    '''
+    Intent: creates the watchlist frame for the login GUI
+    * Preconditions: master is connected to TKinter.
+    * Postconditions:
+    * Post0. watchlist frame is created.
+    '''
+    def createWatchlistPortfolioFrame(self):
         pass
 
-    def createWatchlistPortfolioFrame(self):
-        self.buttons= Frame(self.master, width = 500, height = 400, borderwidth=2, relief="sunken").grid(row = 1,column=1,pady=4)
 
     """Logout button is pressed. 
     Tells Dashboard controller to do logout processing"""
@@ -31,9 +52,6 @@ class DashboardGUI():
         self.DashboardControllerObject.handleLogoutEvent()
         self.closeWindow()
         self.loginlogout_ControllerObject.createLoginGUI()
-    
-
-
 
     def handle_watchlist_event(self):
         pass
