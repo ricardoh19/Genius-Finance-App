99bbad8673e4997bd2cca44bb648fe368ded6014
james hinton <james4pokemon@gmail.com>
2021-10-24 18:40:24 +0000

    Delete portfolio_GUI.py

--- portfolio_GUI.py
+++
@@ -1,8 +0,0 @@
-class PortfolioGUI():
-    def __init__(self, portfolio_controller, stock_price_dict, portfolio_value):
-        self.portfolio_controller = portfolio_controller
-        self.portfolio_value = portfolio_value
-        self.stock_price_dict = stock_price_dict
-
-
-
