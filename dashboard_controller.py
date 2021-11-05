756920780e89f5d60e5159bdf0fc46c6f907ed5c
Ricardo Hernandez <hs.ricardoj@gmail.com>
2021-11-05 14:21:00 -0400

    updated dashboard controller

--- dashboard_controller.py
+++ dashboard_controller.py
@@ -1,34 +1,42 @@
 from stock_controller import StockController
 from yahoo_api import YahooAPI
 from tkinter import *
-import loginlogout_controller
+import loginlogout_controller,dashboardGUI
 from portfolio_controller import PortfolioController
 from watchlist_controller import WatchlistController
 import user
-import popupGUI
+from popupGUI import PopUpGUI
 
 class DashboardController():
-    """This class is the intersection of APP traffic after login. """
+    """This class is the intersection of App traffic after login. """
     def __init__(self):
-        self.login_logout_controller = loginlogout_controller
+        self.loginlogout_controller = loginlogout_controller.LoginLogoutControllers()
         #self.user_object = user.User()
-        
         #created in this class objects
         self.watchlist_object = None 
         self.stock_controller_object= None #replaced by stock object
         self.portfolio_object = None
         #created during initialization object
         self.yahoo_api_object = YahooAPI() # is created
-        
-        
-        
     
-    def handleLogoutEvent(self):
+    
+    def logOutPushChanges(self):
         """In Dashboard GUI logout button is pressed.
         The controller passes the control back to login/logout controller.
         Login logout controller function is called which pushes the changes to the database"""
-        self.login_logout_controller.logout_push_changes_to_database(self.user_object)
+        self.loginlogout_controller.logout_push_changes_to_database()
         
+    def create_portfolio_controller(self):
+        """if portfolio controller object does not exist create it.
+        calls method in Portfolio Controller to create Portfolio GUI."""
+        if self.portfolio_object == None:
+           self.portfolio_object =  PortfolioController(self.user_object,self,self.yahoo_api_object, self.stock_controller_object, self.yahoo_api_object) 
+        self.portfolio_object.create_portfolio_GUI_object()
+        
+
+
+
+
 
     def create_stock_controller(self, stock_symbol):
         """if stock controller object does not exist creates it.
@@ -45,22 +53,16 @@ class DashboardController():
                  self.popup_GUI_object,self)
         #call watchlist GUI in Watchlist controller
         
-    def create_portfolio_controller(self):
-        """if portfolio controller object does not exist create it.
-        calls method in Portfolio Controller to create Portfolio GUI."""
-        if self.portfolio_object == None:
-           self.portfolio_object =  PortfolioController(self.user_object,self,self.yahoo_api_object, self.stock_controller_object, self.yahoo_api_object) 
-        self.portfolio_object.create_portfolio_GUI_object()
-        
+
+
     """This function creates the Dashboard GUI Object"""
     def createDashboardGUI(self):
-        #self.DashboardGUIObject.create
         root = Tk()
-        root.geometry("675x600")
-        self.dashboardGUIObject = dashboard_controller.dashboardGUI(root)
+        root.geometry("600x450")
+        self.dashboardGUIObject = dashboardGUI.DashboardGUI(root)
         root.mainloop()
 
-    def handle_search_bar_event(self, stock_symbol):
+    def searchStockSymbol(self, stock_symbol):
         """Checks if the stock_symbol given in the search bar actually exists.
         If it exists turns over control to Stock controller.
         If it doesn't exist create a pop-up GUI with error message."""
@@ -70,10 +72,8 @@ class DashboardController():
             self.create_stock_controller(stock_symbol)
         else:
             #could not find stock_symbol
-            message = "Could not find stock entered in search bar."
-            self.create_popup_GUI(message)
+            popupGUI = PopUpGUI("Could not find stock entered in search bar.")
+            popupGUI.createPopUp()
 
-    def createPopUpGUI(self, message):
-        """creates a pop-up GUI with given error message."""
-        self.popup_GUI_object.create_pop_up(message)
+    
       
