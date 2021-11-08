ca536717d2694e4d472b046a87cd3c4bc35a1082
Ricardo Hernandez <hs.ricardoj@gmail.com>
2021-11-08 07:57:38 -0500

    updated portfolio controller

--- portfolio_controller.py
+++ portfolio_controller.py
@@ -1,18 +1,19 @@
 from watchlist_controller import WatchlistController
-from portfolio_GUI import PortfolioGUI
+import portfolio_GUI 
 from stock_controller import StockController
+from tkinter import *
 
 class PortfolioController():
-    def __init__(self, user_object,DashboardControllerObject,
-                 YahooAPIObject, StockController, PopUpGUIObject):
+    def __init__(self, user_object, dashboardControllerObject,
+                 yahooAPIObject, stockController,popUpGUIObject):
         #passed in objects
         self.user_object = user_object
-        self.DashboardControllerObject = DashboardControllerObject
-        self.YahooAPIObject = YahooAPIObject
-        self.StockController=StockController
-        self.PopUpGUIObject = PopUpGUIObject
+        self.dashboardControllerObject = dashboardControllerObject
+        self.yahooAPIObject = yahooAPIObject
+        self.stockController=stockController
+        self.popUpGUIObject = popUpGUIObject
         #to be created objects
-        self.WatchlistControllerObject = None
+        self.watchlistControllerObject = None
         self.portfolioGUI = None
         #stock price dictionary 
         self.stocksymbol_price_change_dict = {} 
@@ -32,7 +33,7 @@ class PortfolioController():
     def calculate_percentage_change(self, stocksymbol):
         """I messed up and forgot to pull the percentage change from api. 
         But we can use get_chart method to pull stock value over the past 24 h to compute the percentage change."""
-        stockchart = self.YahooAPIObject.get_stock_graph_values(stocksymbol) #2D list list1: 40 float x values and list2: 40 float y values #x values are float timestamps and y values are float stockprices"
+        stockchart = self.yahooAPIObject.get_stock_graph_values(stocksymbol) #2D list list1: 40 float x values and list2: 40 float y values #x values are float timestamps and y values are float stockprices"
         currentstock_price = float(stockchart[1][0]) # get first entry of stockprices
         stock_price_24h_ago = float(stockchart[1][39]) #get last entry of stockprices
         stock_price_change =  (currentstock_price-stock_price_24h_ago)/stock_price_24h_ago
@@ -42,18 +43,19 @@ class PortfolioController():
         """Removes stock from user object. Therefore it is removed from Portfolio"""
         successful = self.user_object.delete_stock(stocksymbol)
         if not successful: #if we programmed right this should never be exceuted
-            self.create_popup_GUI("Oh no your stock could not be deleted.")
+            self.create_popup_GUI("Your stock could not be deleted.")
             
     def Create_stock_controller_object(self, stocksymbol):
         """Creates Stock Controller Object based on given stocksymbol"""
-        self.StockController = StockController((stocksymbol,self.user_object, self.DashboardControllerObject, self.PopUpGUIObject, self.YahooAPIObject))
+        #self.StockController = StockController((stocksymbol,self.user_object, self.DashboardControllerObject, self.PopUpGUIObject, self.YahooAPIObject))
+
 
     def get_stock_price_yahoo_api_object(self):
         """Get stockinfo dict from Yahoo API. key = stocksymbol, value = current stock price"""
         #retrieve list of all users stocksymbols that will be put in the portfolio
         stock_symbol_list = self.user_object.return_users_stock_symbols()
         #get the stockinfo on each symbol >> here we need price
-        stockinfo = self.YahooAPIObject.get_stock_price_yahoo_api_object(stock_symbol_list)
+        stockinfo = self.yahooAPIObject.get_stock_price_yahoo_api_object(stock_symbol_list)
         #extract just the stockprice associated with the stock and get percentage change on stock over past 24h
         for key in stockinfo:
             percentage_change = self.calculate_percentage_change(stockinfo[key]) #retrieves percentage change over past 24h
@@ -61,17 +63,20 @@ class PortfolioController():
             self.stocksymbol_price_change_dict[key] = \
                 {"stockprice":stockprice, "percentage_change": percentage_change} #updates self.StockPriceDict
         
-    def create_portfolio_GUI_object(self):
-        """Creates Portfolio GUI with """
+    def create_portfolio_GUI(self):
+        """Creates Portfolio GUI """
         #get updated stockprice for each stock that will be displayed in GUI
-        self.get_stock_price_yahoo_api_object()
-        self.portfolioGUI = PortfolioGUI(self, self.stocksymbol_price_change_dict, self.portfolio_value)
+        #self.get_stock_price_yahoo_api_object()
+        root = Tk()
+        root.geometry("675x600")
+        self.portfolioGUI = portfolio_GUI.PortfolioGUI(root)
+        root.mainloop()
 
 
     def CreateWatchListController(self):
         """Creates Watchlist Controlller Object"""
-        self.WatchlistControllerObject = WatchlistController(self.user_object, self.YahooAPIObject,
-                 self.popup_GUI_object,self.DashboardControllerObject)
+        self.WatchlistControllerObject = WatchlistController(self.user_object, self.yahooAPIObject,
+                 self.popup_GUI_object,self.dashboardControllerObject)
 
     def create_popup_GUI(self, message):
         """creates a pop-up GUI with given error message."""
