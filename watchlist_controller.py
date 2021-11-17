eee2d2f9889179ef229c92175aea04f295d2826f
Ricardo Hernandez <hs.ricardoj@gmail.com>
2021-11-17 13:01:03 -0500

    updated watchlist controller

--- watchlist_controller.py
+++ watchlist_controller.py
@@ -1,32 +1,30 @@
-from watchlist_GUI import WatchlistGUI
+import watchlist_GUI
+import yahoo_api
+import popupGUI
+import dashboard_controller
+from tkinter import *
 
 class WatchlistController():
-    def __init__(self, user_object, YahooAPIObject,
-                 popup_GUI_object,DashboardControllerObject):
+    def __init__(self, user_object):
         #passed in objects
         self.user_object = user_object
-        self.YahooAPIObject = YahooAPIObject
-        self.popup_GUI_object = popup_GUI_object
-        self.DashboardControllerObject = DashboardControllerObject
+        self.YahooAPIObject = yahoo_api.YahooAPI()
+        self.popup_GUI_object = popupGUI.PopUpGUI("None")
+        self.dashboardControllerObject = dashboard_controller.DashboardController(self.user_object)
 
         #to be calculated by calling methods in the class: 
-        self.get_stock_info_yahoo_api_object() #get all stockinfo from Yahoo API    
-        self.StockPercentageChange = self.calculate_percentage_change()
+        self.stockinfo = self.get_stock_info_yahoo_api_object() #get all stockinfo from Yahoo API    
+        #self.StockPercentageChange = self.calculate_percentage_change()
         self.best_stocks = []
         self.worst_stocks = []  
-        self.calculate_watchlist_stock_trend()      
+        #self.calculate_watchlist_stock_trend()      
         #string: description
         self.description = "description"
         
-        self.WatchlistGUIObject= self.create_watchlist_GUI()
 
 
     def get_stock_info_yahoo_api_object(self):
         """Get stockinfo dict from Yahoo API. key = stocksymbol, value = current stock price"""
-        #retrieve list of all users stocksymbols that will be put in the portfolio
-        stock_symbol_list = self.user_object.return_users_stock_symbols()      
-        #get the stockinfo on each symbol >> here we need price
-        self.stockinfo = self.YahooAPIObject.get_stock_price_yahoo_api_object(stock_symbol_list)
         # Returns this:
         #   stockinfo[stock_symbol] = {
                 #     "currentRatio": current_ratio,
@@ -35,6 +33,12 @@ class WatchlistController():
                 #     "DebtToEquityRatio": debt_to_equity_ratio,
                 #     "stockPrice": stockprice
                 # } 
+        #retrieve list of all users stocksymbols that will be put in the portfolio
+        stock_symbol_list = self.user_object.return_users_stock_symbols()      
+        #get the stockinfo on each symbol >> here we need price
+        self.stockinfo = self.YahooAPIObject.get_stocks_info(stock_symbol_list)
+        return self.stockinfo
+        
   
     
     def calculate_stocks_percentage_change(self, stocksymbol):
@@ -42,16 +46,18 @@ class WatchlistController():
         But we can use get_chart method to pull stock value over the past 24 h to compute the percentage change."""
         stockchart = self.YahooAPIObject.get_stock_graph_values(stocksymbol) #2D list list1: 40 float x values and list2: 40 float y values #x values are float timestamps and y values are float stockprices"
         currentstock_price = float(stockchart[1][0]) # get first entry of stockprices
-        stock_price_24h_ago = float(stockchart[1][39]) #get last entry of stockprices
+        stock_price_24h_ago = float(stockchart[1][-1]) #get last entry of stockprices
         stock_price_change =  (currentstock_price-stock_price_24h_ago)/stock_price_24h_ago
         return stock_price_change*100 #percentage change of stock price over past 24 h
         
     def calculate_percentage_change(self):
-        for stocksymbol in self.stockinfo.keys():
-            percentage_change = self.calculate_percentage_change(stocksymbol)
-            self.stockinfo['PercentageChange'] = percentage_change
+        self.stockinfo = self.get_stock_info_yahoo_api_object()
+        for stocksymbol in list(self.stockinfo):
+            percentage_change = self.calculate_stocks_percentage_change(stocksymbol)
+            self.stockinfo[stocksymbol]['PercentageChange'] = percentage_change
+        return self.stockinfo
 
-    def calculate_watchlist_stock_trend(self):
+    def calculate_watchlist_stock_trend(self, stockinfo):
         """This function sorts the stocks according the percentage change.
         The best 3 stocks are added to best 3 stocks, the worst three in the worst 3 stocks.
         If there are less than 6 stocks they are divided into best and worst in the middle.
@@ -60,8 +66,9 @@ class WatchlistController():
         self.worst_stocks = []
         all_stocks = []
         #get a 2D list with stocksymbol and percentage change
-        for stocksymbol in self.stockinfo.keys():
-            all_stocks.append([stocksymbol, self.stockinfo[stocksymbol]['PercentageChange']])
+        
+        for stocksymbol in stockinfo.keys():
+            all_stocks.append([stocksymbol, stockinfo[stocksymbol]['PercentageChange']])
         #sort stocks according to percentage change
         all_stocks = sorted(all_stocks, key=lambda x: x[1])
         print(all_stocks)
@@ -77,9 +84,21 @@ class WatchlistController():
            print("Error no stocks in Portfolio.")
            self.create_popup_GUI("Error no stocks in Portfolio.")
                  
+        return self.best_stocks, self.worst_stocks
+
     def create_watchlist_GUI(self):
-        return WatchlistGUI(self, self.popup_GUI_object, self.StockTrendingUp,
-                 self.StocksTrendingDown, self.description, self.StockPercentageChange)
+        stockInfo = self.calculate_percentage_change()
+        stocksTrendingUp,stocksTrendingDown = self.calculate_watchlist_stock_trend(stockInfo)
+        
+        description = None
+        print(f"stocks trending up: {stocksTrendingUp}")
+        print(f"stocks trending up: {stocksTrendingDown}")
+        
+        root = Tk()
+        root.geometry("750x600")
+        self.portfolioGUI = watchlist_GUI.WatchlistGUI(root, self.user_object,stocksTrendingUp, 
+        stocksTrendingDown, description, stockInfo )
+        root.mainloop()
     
     def create_popup_GUI(self, message):
         """creates a pop-up GUI with given error message."""
